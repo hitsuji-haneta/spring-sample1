a9b51bcb79eade2fc916656f7d5ebc9aeb1497af
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2024-06-26 14:55:38 +0800

    add RiskyDeposit.sol

---
+++ src/RiskyDonation.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Target {
+    address owner;
+    uint256 public bonus = 0;
+    mapping(address => uint256[]) public balances;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function donate(address _to) public payable {
+        balances[_to].push(msg.value + bonus*1e18);
+    }
+
+    function balanceOf(address _who) public view returns (uint256 balance) {
+        for(uint i = 0; i < balances[_who].length; i++) {
+            balance += balances[_who][i];
+        }
+        return balance;
+    }
+
+    function increment() public onlyOwner {
+        unchecked {
+            bonus++;
+        }
+    }
+
+    function withdraw(uint256 _amount) public {
+        if (balanceOf(msg.sender) >= _amount) {
+            (bool result, ) = msg.sender.call{value: _amount}("");
+            require(result, "Transfer failed.");
+            _withdraw(_amount);
+        }
+    }
+
+    function _withdraw(uint256 _amount) private {
+        uint256 remaining = _amount;
+        for(uint i = 0; i < balances[msg.sender].length; i++) {
+            remaining -= balances[msg.sender][i];
+            if (remaining > balances[msg.sender][i]) {
+                balances[msg.sender][i] = 0;
+                remaining -= balances[msg.sender][i];
+            } else {
+                balances[msg.sender][i] -= remaining;
+                remaining = 0;
+            }
+        }
+    }
+
+    receive() external payable {}
+    fallback() external payable {}
+}
