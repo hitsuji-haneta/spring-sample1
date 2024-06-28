b58f7aee9e46d6d3cd6fc3bc64c309d4d52499ed
hitsuji-haneta <hitsuji-haneta@hotmail.com>
2024-06-28 11:41:53 +0800

    update Counter

--- src/Counter.sol
+++ src/Counter.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.13;
 contract Counter {
     uint256 public number;
 
+    constructor(uint256 _initialNumber) {
+      number = _initialNumber;
+    }
+
     function setNumber(uint256 newNumber) public {
         number = newNumber;
     }
